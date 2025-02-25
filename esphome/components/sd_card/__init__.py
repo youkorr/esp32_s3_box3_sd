d76cec8c865575b8f9fd040f72295bd7d98deb13
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-25 22:59:15 +0100

    Update __init__.py

--- esphome/components/sd_card/__init__.py
+++ esphome/components/sd_card/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
-from esphome.const import CONF_ID, CONF_PATH
+from esphome.const import CONF_ID, CONF_PATH, CONF_AUTO_MOUNT
 from esphome.core import CORE
 
 # Déclaration des composants
@@ -10,7 +10,7 @@ SdMmc = sd_mmc_card_ns.class_("SdMmc", cg.Component)
 
 # Déclaration des actions
 SdMmcPlayAudioAction = sd_mmc_card_ns.class_("SdMmcPlayAudioAction", automation.Action)
-SdMmcDisplayPngAction = sd_mmc_card_ns.class_("SdMmcDisplayPngAction", automation.Action)
+SdMmcLoadImageAction = sd_mmc_card_ns.class_("SdMmcLoadImageAction", automation.Action)
 
 # Schéma de configuration pour l'action de lecture audio
 SD_MMC_PLAY_AUDIO_ACTION_SCHEMA = cv.Schema({
@@ -18,11 +18,22 @@ SD_MMC_PLAY_AUDIO_ACTION_SCHEMA = cv.Schema({
     cv.Required(CONF_PATH): cv.templatable(cv.string_strict),
 })
 
-# Schéma de configuration pour l'action d'affichage d'image
-SD_MMC_DISPLAY_PNG_ACTION_SCHEMA = cv.Schema({
+# Schéma de configuration pour l'action de chargement d'image
+SD_MMC_LOAD_IMAGE_ACTION_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.use_id(SdMmc),
     cv.Required(CONF_PATH): cv.templatable(cv.string_strict),
 })
+
+# Schéma de configuration pour le composant SD Card
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SdMmc),
+    cv.Optional(CONF_AUTO_MOUNT, default=True): cv.boolean,  # Montage automatique activé par défaut
+    cv.Required("mode"): cv.enum({"SPI": "SPI", "MMC": "MMC"}),  # Mode de communication (SPI ou MMC)
+    cv.Required("cs_pin"): cv.gpio_output_pin_schema,  # Broche Chip Select (CS)
+    cv.Optional("mosi_pin"): cv.gpio_output_pin_schema,  # Broche MOSI (Data In)
+    cv.Optional("miso_pin"): cv.gpio_input_pin_schema,  # Broche MISO (Data Out)
+    cv.Required("clk_pin"): cv.gpio_output_pin_schema,  # Broche Clock (CLK)
+}).extend(cv.COMPONENT_SCHEMA)
 
 # Fonction pour enregistrer l'action de lecture audio
 @automation.register_action("sd_mmc_card.play_audio", SdMmcPlayAudioAction, SD_MMC_PLAY_AUDIO_ACTION_SCHEMA)
@@ -33,27 +44,38 @@ async def sd_mmc_play_audio_to_code(config, action_id, template_arg, args):
     cg.add(var.set_path(path_))
     return var
 
-# Fonction pour enregistrer l'action d'affichage d'image
-@automation.register_action("sd_mmc_card.display_png", SdMmcDisplayPngAction, SD_MMC_DISPLAY_PNG_ACTION_SCHEMA)
-async def sd_mmc_display_png_to_code(config, action_id, template_arg, args):
+# Fonction pour enregistrer l'action de chargement d'image
+@automation.register_action("sd_mmc_card.load_image", SdMmcLoadImageAction, SD_MMC_LOAD_IMAGE_ACTION_SCHEMA)
+async def sd_mmc_load_image_to_code(config, action_id, template_arg, args):
     parent = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, parent)
     path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
     cg.add(var.set_path(path_))
     return var
 
-# Fonction pour monter la carte SD et lire un fichier audio ou afficher une image au démarrage
+# Fonction pour monter la carte SD au démarrage
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Montage de la carte SD
-    cg.add(var.setup())
+    # Configuration des broches
+    cs_pin = await cg.gpio_pin_expression(config["cs_pin"])
+    cg.add(var.set_cs_pin(cs_pin))
 
-    # Lecture d'un fichier audio ou affichage d'une image au démarrage
-    if CORE.is_esp32:
-        cg.add(var.play_audio("/test.flac"))  # Lire un fichier audio
-        cg.add(var.display_png("/test.png"))  # Afficher une image
+    clk_pin = await cg.gpio_pin_expression(config["clk_pin"])
+    cg.add(var.set_clk_pin(clk_pin))
+
+    if "mosi_pin" in config:
+        mosi_pin = await cg.gpio_pin_expression(config["mosi_pin"])
+        cg.add(var.set_mosi_pin(mosi_pin))
+
+    if "miso_pin" in config:
+        miso_pin = await cg.gpio_pin_expression(config["miso_pin"])
+        cg.add(var.set_miso_pin(miso_pin))
+
+    # Montage automatique de la carte SD
+    if config[CONF_AUTO_MOUNT]:
+        cg.add(var.setup())
 
 
 
