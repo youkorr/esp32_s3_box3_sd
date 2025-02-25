9242541600b585ab9fb03324d5f3f0b00f0d5aae
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-25 01:07:35 +0100

    Update __init__.py

--- esphome/components/sd_card/__init__.py
+++ esphome/components/sd_card/__init__.py
@@ -1,22 +1,4 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import spi
-from esphome.const import CONF_ID
 
-DEPENDENCIES = ['spi']
-AUTO_LOAD = ['spi']
-
-sdcard_ns = cg.esphome_ns.namespace('sdcard')
-SDCard = sdcard_ns.class_('SDCard', cg.Component, spi.SPIDevice)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SDCard),
-}).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema())
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield spi.register_spi_device(var, config)
 
 
 
