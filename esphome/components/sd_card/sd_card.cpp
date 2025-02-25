b761470f892ba29c04948363535c9bc24ef7bc43
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-25 21:54:19 +0100

    Update sd_card.cpp

--- esphome/components/sd_card/sd_card.cpp
+++ esphome/components/sd_card/sd_card.cpp
@@ -1,24 +1,28 @@
 #include "sd_card.h"
-
 #include <algorithm>
-
-#include "math.h"
 #include "esphome/core/log.h"
+#include "FS.h"
+#include "SD_MMC.h"
 
 namespace esphome {
 namespace sd_card {
 
 static const char *TAG = "sd_card";
 
-#ifdef USE_SENSOR
-FileSizeSensor::FileSizeSensor(sensor::Sensor *sensor, std::string const &path) : sensor(sensor), path(path) {}
-#endif
+void SdMmc::setup() {
+  if (!SD_MMC.begin()) {
+    this->mark_failed();
+    ESP_LOGE(TAG, "Failed to mount SD card");
+    return;
+  }
+  ESP_LOGI(TAG, "SD card mounted successfully");
+}
 
 void SdMmc::loop() {}
 
 void SdMmc::dump_config() {
   ESP_LOGCONFIG(TAG, "SD MMC Component");
-  ESP_LOGCONFIG(TAG, "  Mode 1 bit: %s", TRUEFALSE(this->mode_1bit_));
+  ESP_LOGCONFIG(TAG, "  Mode 1 bit: %s", this->mode_1bit_ ? "true" : "false");
   ESP_LOGCONFIG(TAG, "  CLK Pin: %d", this->clk_pin_);
   ESP_LOGCONFIG(TAG, "  CMD Pin: %d", this->cmd_pin_);
   ESP_LOGCONFIG(TAG, "  DATA0 Pin: %d", this->data0_pin_);
@@ -27,39 +31,30 @@ void SdMmc::dump_config() {
     ESP_LOGCONFIG(TAG, "  DATA2 Pin: %d", this->data2_pin_);
     ESP_LOGCONFIG(TAG, "  DATA3 Pin: %d", this->data3_pin_);
   }
-#ifdef USE_SENSOR
-  LOG_SENSOR("  ", "Used space", this->used_space_sensor_);
-  LOG_SENSOR("  ", "Total space", this->total_space_sensor_);
-  LOG_SENSOR("  ", "Free space", this->free_space_sensor_);
-  for (auto &sensor : this->file_size_sensors_) {
-    if (sensor.sensor != nullptr)
-      LOG_SENSOR("  ", "File size", sensor.sensor);
-  }
-#endif
-#ifdef USE_TEXT_SENSOR
-  LOG_TEXT_SENSOR("  ", "SD Card Type", this->sd_card_type_text_sensor_);
-#endif
+}
 
-  if (this->is_failed()) {
-    ESP_LOGE(TAG, "Setup failed : %s", SdMmc::error_code_to_string(this->init_error_).c_str());
+void SdMmc::write_file(const char *path, const uint8_t *buffer, size_t len, const char *mode) {
+  File file = SD_MMC.open(path, mode);
+  if (!file) {
+    ESP_LOGE(TAG, "Failed to open file for writing: %s", path);
     return;
   }
+  file.write(buffer, len);
+  file.close();
 }
 
 void SdMmc::write_file(const char *path, const uint8_t *buffer, size_t len) {
-  ESP_LOGV(TAG, "Writing to file: %s", path);
   this->write_file(path, buffer, len, "w");
 }
 
 void SdMmc::append_file(const char *path, const uint8_t *buffer, size_t len) {
-  ESP_LOGV(TAG, "Appending to file: %s", path);
   this->write_file(path, buffer, len, "a");
 }
 
 std::vector<std::string> SdMmc::list_directory(const char *path, uint8_t depth) {
   std::vector<std::string> list;
-  std::vector<FileInfo> infos = list_directory_file_info(path, depth);
-  std::transform(infos.cbegin(), infos.cend(), list.begin(), [](FileInfo const &info) { return info.path; });
+  std::vector<FileInfo> infos = this->list_directory_file_info(path, depth);
+  std::transform(infos.begin(), infos.end(), std::back_inserter(list), [](FileInfo const &info) { return info.path; });
   return list;
 }
 
@@ -69,7 +64,7 @@ std::vector<std::string> SdMmc::list_directory(std::string path, uint8_t depth) 
 
 std::vector<FileInfo> SdMmc::list_directory_file_info(const char *path, uint8_t depth) {
   std::vector<FileInfo> list;
-  list_directory_file_info_rec(path, depth, list);
+  this->list_directory_file_info_rec(path, depth, list);
   return list;
 }
 
@@ -77,33 +72,66 @@ std::vector<FileInfo> SdMmc::list_directory_file_info(std::string path, uint8_t 
   return this->list_directory_file_info(path.c_str(), depth);
 }
 
-size_t SdMmc::file_size(std::string const &path) { return this->file_size(path.c_str()); }
+void SdMmc::list_directory_file_info_rec(const char *path, uint8_t depth, std::vector<FileInfo> &list) {
+  File root = SD_MMC.open(path);
+  if (!root) {
+    ESP_LOGE(TAG, "Failed to open directory: %s", path);
+    return;
+  }
+  if (!root.isDirectory()) {
+    ESP_LOGE(TAG, "Not a directory: %s", path);
+    return;
+  }
 
-bool SdMmc::is_directory(std::string const &path) { return this->is_directory(path.c_str()); }
-
-bool SdMmc::delete_file(std::string const &path) { return this->delete_file(path.c_str()); }
-
-std::vector<uint8_t> SdMmc::read_file(std::string const &path) { return this->read_file(path.c_str()); }
-
-#ifdef USE_SENSOR
-void SdMmc::add_file_size_sensor(sensor::Sensor *sensor, std::string const &path) {
-  this->file_size_sensors_.emplace_back(sensor, path);
+  File file = root.openNextFile();
+  while (file) {
+    list.emplace_back(file.name(), file.size(), file.isDirectory());
+    if (depth > 0 && file.isDirectory()) {
+      this->list_directory_file_info_rec(file.name(), depth - 1, list);
+    }
+    file = root.openNextFile();
+  }
+  root.close();
 }
-#endif
 
-void SdMmc::set_clk_pin(uint8_t pin) { this->clk_pin_ = pin; }
+size_t SdMmc::file_size(std::string const &path) {
+  File file = SD_MMC.open(path.c_str());
+  if (!file) {
+    ESP_LOGE(TAG, "Failed to open file: %s", path.c_str());
+    return 0;
+  }
+  size_t size = file.size();
+  file.close();
+  return size;
+}
 
-void SdMmc::set_cmd_pin(uint8_t pin) { this->cmd_pin_ = pin; }
+bool SdMmc::is_directory(std::string const &path) {
+  File file = SD_MMC.open(path.c_str());
+  if (!file) {
+    ESP_LOGE(TAG, "Failed to open file: %s", path.c_str());
+    return false;
+  }
+  bool is_dir = file.isDirectory();
+  file.close();
+  return is_dir;
+}
 
-void SdMmc::set_data0_pin(uint8_t pin) { this->data0_pin_ = pin; }
+bool SdMmc::delete_file(std::string const &path) {
+  return SD_MMC.remove(path.c_str());
+}
 
-void SdMmc::set_data1_pin(uint8_t pin) { this->data1_pin_ = pin; }
-
-void SdMmc::set_data2_pin(uint8_t pin) { this->data2_pin_ = pin; }
-
-void SdMmc::set_data3_pin(uint8_t pin) { this->data3_pin_ = pin; }
-
-void SdMmc::set_mode_1bit(bool b) { this->mode_1bit_ = b; }
+std::vector<uint8_t> SdMmc::read_file(std::string const &path) {
+  std::vector<uint8_t> data;
+  File file = SD_MMC.open(path.c_str());
+  if (!file) {
+    ESP_LOGE(TAG, "Failed to open file: %s", path.c_str());
+    return data;
+  }
+  data.resize(file.size());
+  file.read(data.data(), data.size());
+  file.close();
+  return data;
+}
 
 std::string SdMmc::error_code_to_string(SdMmc::ErrorCode code) {
   switch (code) {
@@ -118,12 +146,14 @@ std::string SdMmc::error_code_to_string(SdMmc::ErrorCode code) {
   }
 }
 
-long double convertBytes(uint64_t value, MemoryUnits unit) {
-  return value * 1.0 / pow(1024, static_cast<uint64_t>(unit));
-}
-
 FileInfo::FileInfo(std::string const &path, size_t size, bool is_directory)
     : path(path), size(size), is_directory(is_directory) {}
+
+#ifdef USE_SENSOR
+void SdMmc::add_file_size_sensor(sensor::Sensor *sensor, std::string const &path) {
+  this->file_size_sensors_.emplace_back(sensor, path);
+}
+#endif
 
 }  // namespace sd_card
 }  // namespace esphome
