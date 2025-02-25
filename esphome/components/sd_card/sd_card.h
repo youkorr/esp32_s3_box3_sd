632881560050f56b3eeb85edf1dd4166ffcc1c4e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-25 21:53:43 +0100

    Update sd_card.h

--- esphome/components/sd_card/sd_card.h
+++ esphome/components/sd_card/sd_card.h
@@ -1,35 +1,88 @@
-#pragma once
+#ifndef SD_CARD_H
+#define SD_CARD_H
 
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
 #include "esphome/components/sensor/sensor.h"
-#include <SD.h>
-#include <SPI.h>
+#include "esphome/components/text_sensor/text_sensor.h"
+#include <vector>
+#include <string>
 
 namespace esphome {
-namespace esp32_s3_box3_sd {
+namespace sd_card {
 
-class ESP32S3Box3SDCard : public Component {
+class SdMmc : public Component {
  public:
   void setup() override;
   void loop() override;
   void dump_config() override;
-  
-  void set_space_used_sensor(sensor::Sensor *space_used_sensor) { space_used_sensor_ = space_used_sensor; }
-  void set_total_space_sensor(sensor::Sensor *total_space_sensor) { total_space_sensor_ = total_space_sensor; }
+
+  void write_file(const char *path, const uint8_t *buffer, size_t len);
+  void append_file(const char *path, const uint8_t *buffer, size_t len);
+  std::vector<std::string> list_directory(const char *path, uint8_t depth = 0);
+  std::vector<std::string> list_directory(std::string path, uint8_t depth = 0);
+  std::vector<FileInfo> list_directory_file_info(const char *path, uint8_t depth = 0);
+  std::vector<FileInfo> list_directory_file_info(std::string path, uint8_t depth = 0);
+  size_t file_size(std::string const &path);
+  bool is_directory(std::string const &path);
+  bool delete_file(std::string const &path);
+  std::vector<uint8_t> read_file(std::string const &path);
+
+  void set_clk_pin(uint8_t pin) { clk_pin_ = pin; }
+  void set_cmd_pin(uint8_t pin) { cmd_pin_ = pin; }
+  void set_data0_pin(uint8_t pin) { data0_pin_ = pin; }
+  void set_data1_pin(uint8_t pin) { data1_pin_ = pin; }
+  void set_data2_pin(uint8_t pin) { data2_pin_ = pin; }
+  void set_data3_pin(uint8_t pin) { data3_pin_ = pin; }
+  void set_mode_1bit(bool b) { mode_1bit_ = b; }
+
+  enum class ErrorCode {
+    ERR_PIN_SETUP,
+    ERR_MOUNT,
+    ERR_NO_CARD,
+  };
+
+  static std::string error_code_to_string(ErrorCode code);
+
+  struct FileInfo {
+    std::string path;
+    size_t size;
+    bool is_directory;
+
+    FileInfo(std::string const &path, size_t size, bool is_directory);
+  };
+
+#ifdef USE_SENSOR
+  void add_file_size_sensor(sensor::Sensor *sensor, std::string const &path);
+#endif
 
  protected:
-  void update_sensors_();
+  void write_file(const char *path, const uint8_t *buffer, size_t len, const char *mode);
+  void list_directory_file_info_rec(const char *path, uint8_t depth, std::vector<FileInfo> &list);
 
-  sensor::Sensor *space_used_sensor_{nullptr};
-  sensor::Sensor *total_space_sensor_{nullptr};
+  uint8_t clk_pin_;
+  uint8_t cmd_pin_;
+  uint8_t data0_pin_;
+  uint8_t data1_pin_;
+  uint8_t data2_pin_;
+  uint8_t data3_pin_;
+  bool mode_1bit_ = false;
 
-  static const int SD_CS = 10;   // Chip Select (vérifiez la documentation du BOX3)
-  static const int SD_SCK = 12;  // Clock
-  static const int SD_MOSI = 11; // MOSI
-  static const int SD_MISO = 13; // MISO
+#ifdef USE_SENSOR
+  struct FileSizeSensor {
+    sensor::Sensor *sensor;
+    std::string path;
+
+    FileSizeSensor(sensor::Sensor *sensor, std::string const &path);
+  };
+
+  std::vector<FileSizeSensor> file_size_sensors_;
+#endif
 };
 
-}  // namespace esp32_s3_box3_sd
+}  // namespace sd_card
 }  // namespace esphome
+
+#endif  // SD_CARD_H
 
 
