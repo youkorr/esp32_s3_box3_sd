e116e9b4bb5d854d44598b58f5db5514d33e245c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 23:03:22 +0100

    Update __init__.py

--- esphome/components/sd_mmc_card/__init__.py
+++ esphome/components/sd_mmc_card/__init__.py
@@ -18,11 +18,12 @@ CONF_DATA1_PIN = "data1_pin"
 CONF_DATA2_PIN = "data2_pin"
 CONF_DATA3_PIN = "data3_pin"
 CONF_MODE_1BIT = "mode_1bit"
+CONF_MOUNT_POINT = "mount_point"
 
 sd_mmc_card_component_ns = cg.esphome_ns.namespace("sd_mmc_card")
 SdMmc = sd_mmc_card_component_ns.class_("SdMmc", cg.Component)
 
-# Action
+# Actions
 SdMmcWriteFileAction = sd_mmc_card_component_ns.class_("SdMmcWriteFileAction", automation.Action)
 SdMmcAppendFileAction = sd_mmc_card_component_ns.class_("SdMmcAppendFileAction", automation.Action)
 SdMmcCreateDirectoryAction = sd_mmc_card_component_ns.class_("SdMmcCreateDirectoryAction", automation.Action)
@@ -34,9 +35,7 @@ def validate_raw_data(value):
         return value.encode("utf-8")
     if isinstance(value, list):
         return cv.Schema([cv.hex_uint8_t])(value)
-    raise cv.Invalid(
-        "data must either be a string wrapped in quotes or a list of bytes"
-    )
+    raise cv.Invalid("data must either be a string wrapped in quotes or a list of bytes")
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -48,53 +47,54 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
         cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
         cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
+        cv.Optional(CONF_MOUNT_POINT, default="/sdcard"): cv.string,
     }
 ).extend(cv.COMPONENT_SCHEMA)
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
     cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
-
     cg.add(var.set_clk_pin(config[CONF_CLK_PIN]))
     cg.add(var.set_cmd_pin(config[CONF_CMD_PIN]))
     cg.add(var.set_data0_pin(config[CONF_DATA0_PIN]))
 
-    if (config[CONF_MODE_1BIT] == False):
+    if not config[CONF_MODE_1BIT]:
         cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
         cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
         cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
 
-    if CORE.using_arduino:
-        if CORE.is_esp32:
-            cg.add_library("FS", None)
-            cg.add_library("SD_MMC", None)
-            cg.add_library("SD", None)
-    # Montage automatique de la carte SD
+    if CORE.using_arduino and CORE.is_esp32:
+        cg.add_library("FS", None)
+        cg.add_library("SD_MMC", None)
+        cg.add_library("SD", None)
+    
+    mount_point = config[CONF_MOUNT_POINT]
     cg.add_define("USE_SD_MMC")
     cg.add(
-        f"""
-        esp_vfs_fat_sdmmc_mount_config_t mount_config = {{
-            .format_if_mount_failed = false,
-            .max_files = 5,
-            .allocation_unit_size = 16 * 1024
-        }};
-        sdmmc_card_t *card;
-        esp_err_t ret = esp_vfs_fat_sdmmc_mount(
-            {config[CONF_MOUNT_POINT]},
-            &host,
-            &slot_config,
-            &mount_config,
-            &card
-        );
-        if (ret != ESP_OK) {{
-            ESP_LOGE("SDPI", "Failed to mount SD card: %s", esp_err_to_name(ret));
-            return;
-        }}
-        ESP_LOGI("SDPI", "SD card mounted at %s", {config[CONF_MOUNT_POINT]});
-        """
+        cg.RawExpression(
+            f'''
+            esp_vfs_fat_sdmmc_mount_config_t mount_config = {{
+                .format_if_mount_failed = false,
+                .max_files = 5,
+                .allocation_unit_size = 16 * 1024
+            }};
+            sdmmc_card_t *card;
+            esp_err_t ret = esp_vfs_fat_sdmmc_mount(
+                "{mount_point}",
+                &host,
+                &slot_config,
+                &mount_config,
+                &card
+            );
+            if (ret != ESP_OK) {{
+                ESP_LOGE("SDPI", "Failed to mount SD card: %s", esp_err_to_name(ret));
+                return;
+            }}
+            ESP_LOGI("SDPI", "SD card mounted at {mount_point}");
+            '''
+        )
     )
 
 SD_MMC_PATH_ACTION_SCHEMA = cv.Schema(
@@ -112,9 +112,7 @@ SD_MMC_WRITE_FILE_ACTION_SCHEMA = cv.Schema(
     }
 ).extend(SD_MMC_PATH_ACTION_SCHEMA)
 
-@automation.register_action(
-    "sd_mmc_card.write_file", SdMmcWriteFileAction, SD_MMC_WRITE_FILE_ACTION_SCHEMA
-)
+@automation.register_action("sd_mmc_card.write_file", SdMmcWriteFileAction, SD_MMC_WRITE_FILE_ACTION_SCHEMA)
 async def sd_mmc_write_file_to_code(config, action_id, template_arg, args):
     parent = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, parent)
@@ -124,23 +122,7 @@ async def sd_mmc_write_file_to_code(config, action_id, template_arg, args):
     cg.add(var.set_data(data_))
     return var
 
-
-@automation.register_action(
-    "sd_mmc_card.append_file", SdMmcAppendFileAction, SD_MMC_WRITE_FILE_ACTION_SCHEMA
-)
-async def sd_mmc_append_file_to_code(config, action_id, template_arg, args):
-    parent = await cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, parent)
-    path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
-    data_ = await cg.templatable(config[CONF_DATA], args, cg.std_vector.template(cg.uint8))
-    cg.add(var.set_path(path_))
-    cg.add(var.set_data(data_))
-    return var
-
-
-@automation.register_action(
-    "sd_mmc_card.create_directory", SdMmcCreateDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA
-)
+@automation.register_action("sd_mmc_card.create_directory", SdMmcCreateDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA)
 async def sd_mmc_create_directory_to_code(config, action_id, template_arg, args):
     parent = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, parent)
@@ -148,10 +130,7 @@ async def sd_mmc_create_directory_to_code(config, action_id, template_arg, args)
     cg.add(var.set_path(path_))
     return var
 
-
-@automation.register_action(
-    "sd_mmc_card.remove_directory", SdMmcRemoveDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA
-)
+@automation.register_action("sd_mmc_card.remove_directory", SdMmcRemoveDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA)
 async def sd_mmc_remove_directory_to_code(config, action_id, template_arg, args):
     parent = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, parent)
@@ -159,16 +138,14 @@ async def sd_mmc_remove_directory_to_code(config, action_id, template_arg, args)
     cg.add(var.set_path(path_))
     return var
 
-
-@automation.register_action(
-    "sd_mmc_card.delete_file", SdMmcDeleteFileAction, SD_MMC_PATH_ACTION_SCHEMA
-)
+@automation.register_action("sd_mmc_card.delete_file", SdMmcDeleteFileAction, SD_MMC_PATH_ACTION_SCHEMA)
 async def sd_mmc_delete_file_to_code(config, action_id, template_arg, args):
     parent = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, parent)
     path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
     cg.add(var.set_path(path_))
     return var
+
 
 
 
