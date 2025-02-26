2e0ea59c54ee19df85f86b1c438227e6adcc5cb2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 05:13:41 +0100

    Update sd_card.h

--- esphome/components/sd_box_card/sd_card.h
+++ esphome/components/sd_box_card/sd_card.h
@@ -1,28 +1,31 @@
 #pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "sdmmc_cmd.h"
+#include "esphome.h"
 
 namespace esphome {
 namespace sd_box_card {
 
 class SDBoxCard : public Component {
  public:
-  void set_mosi_pin(GPIOPin *pin) { mosi_pin_ = pin; }
-  void set_miso_pin(GPIOPin *pin) { miso_pin_ = pin; }
+  void set_data3_pin(GPIOPin *pin) { data3_pin_ = pin; }
+  void set_cmd_pin(GPIOPin *pin) { cmd_pin_ = pin; }
+  void set_data0_pin(GPIOPin *pin) { data0_pin_ = pin; }
+  void set_data1_pin(GPIOPin *pin) { data1_pin_ = pin; }
+  void set_data2_pin(GPIOPin *pin) { data2_pin_ = pin; }
   void set_clk_pin(GPIOPin *pin) { clk_pin_ = pin; }
-  void set_cs_pin(GPIOPin *pin) { cs_pin_ = pin; }
+  void set_mode_1bit(bool mode) { mode_1bit_ = mode; }
 
   void setup() override;
   void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::LATE; }
 
  protected:
-  GPIOPin *mosi_pin_{nullptr};
-  GPIOPin *miso_pin_{nullptr};
-  GPIOPin *clk_pin_{nullptr};
-  GPIOPin *cs_pin_{nullptr};
-  sdmmc_card_t *card_{nullptr};
+  GPIOPin *data3_pin_;
+  GPIOPin *cmd_pin_;
+  GPIOPin *data0_pin_;
+  GPIOPin *data1_pin_;
+  GPIOPin *data2_pin_;
+  GPIOPin *clk_pin_;
+  bool mode_1bit_;
 };
 
 }  // namespace sd_box_card
