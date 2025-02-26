2b3dd56352dd0433b47de3c74b9a5f06e013fe99
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 17:56:53 +0100

    Update sensor.py

--- esphome/components/sd_card/sensor.py
+++ esphome/components/sd_card/sensor.py
@@ -2,58 +2,53 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_TYPE,
+    DEVICE_CLASS_DATA_SIZE,
+    ENTITY_CATEGORY_DIAGNOSTIC,
     STATE_CLASS_MEASUREMENT,
-    UNIT_BYTES,
-    ICON_MEMORY,
+    UNIT_GIGABYTE,
 )
-from . import (
-    SdMmc,
-    CONF_SD_MMC_CARD_ID,
-    CONF_PATH,
-)
+from . import CONF_SD_BOX_CARD, CONF_FREE_SPACE, CONF_TOTAL_SPACE, CONF_USED_SPACE
 
-DEPENDENCIES = ["sd_mmc_card"]
+DEPENDENCIES = ["sd_card"]
 
-CONF_USED_SPACE = "used_space"
-CONF_TOTAL_SPACE = "total_space"
-CONF_FREE_SPACE = "free_space"
-CONF_FILE_SIZE = "file_size"
-
-TYPES = [CONF_USED_SPACE, CONF_TOTAL_SPACE, CONF_USED_SPACE, CONF_FREE_SPACE]
-SIMPLE_TYPES = [CONF_USED_SPACE, CONF_TOTAL_SPACE, CONF_FREE_SPACE]
-
-BASE_CONFIG_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_BYTES,
-    icon=ICON_MEMORY,
-    accuracy_decimals=2,
-    state_class=STATE_CLASS_MEASUREMENT,
-).extend(
+CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(CONF_SD_MMC_CARD_ID): cv.use_id(SdMmc),
+        cv.GenerateID(CONF_SD_BOX_CARD): cv.use_id(cg.Component),
+        cv.Optional(CONF_TOTAL_SPACE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_GIGABYTE,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_DATA_SIZE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+        cv.Optional(CONF_USED_SPACE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_GIGABYTE,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_DATA_SIZE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+        cv.Optional(CONF_FREE_SPACE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_GIGABYTE,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_DATA_SIZE,
+            state_class=STATE_CLASS_MEASUREMENT,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
     }
 )
 
-CONFIG_SCHEMA = cv.typed_schema(
-    {
-        CONF_TOTAL_SPACE : BASE_CONFIG_SCHEMA,
-        CONF_USED_SPACE : BASE_CONFIG_SCHEMA,
-        CONF_FREE_SPACE: BASE_CONFIG_SCHEMA,
-        CONF_FILE_SIZE: BASE_CONFIG_SCHEMA.extend(
-            {
-                cv.Required(CONF_PATH): cv.templatable(cv.string_strict),
-            }
-        )
-    },
-    lower=True,
-)
-
-
 async def to_code(config):
-    sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_CARD_ID])
-    var = await sensor.new_sensor(config)
-    if config[CONF_TYPE] in SIMPLE_TYPES:
-        func = getattr(sd_mmc_component, f"set_{config[CONF_TYPE]}_sensor")
-        cg.add(func(var))
-    elif config[CONF_TYPE] == CONF_FILE_SIZE:
-        cg.add(sd_mmc_component.add_file_size_sensor(var, config[CONF_PATH]))
+    sd_card = await cg.get_variable(config[CONF_SD_BOX_CARD])
+    
+    if CONF_TOTAL_SPACE in config:
+        sens = await sensor.new_sensor(config[CONF_TOTAL_SPACE])
+        cg.add(sd_card.set_total_space_sensor(sens))
+    
+    if CONF_USED_SPACE in config:
+        sens = await sensor.new_sensor(config[CONF_USED_SPACE])
+        cg.add(sd_card.set_used_space_sensor(sens))
+    
+    if CONF_FREE_SPACE in config:
+        sens = await sensor.new_sensor(config[CONF_FREE_SPACE])
+        cg.add(sd_card.set_free_space_sensor(sens))
