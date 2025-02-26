c57e2b4cec4899f15feb5fd379a96f6932c149d3
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 07:08:22 +0100

    Update automation.h

--- esphome/components/sd_card/automation.h
+++ esphome/components/sd_card/automation.h
@@ -6,22 +6,30 @@
 namespace esphome {
 namespace sd_card {
 
-class SDCardUpdateAction : public Action<> {
+class SDCardUpdateAction : public Action {
  public:
   explicit SDCardUpdateAction(SDCard *sd_card) : sd_card_(sd_card) {}
   
-  void play(Ts...) override { this->sd_card_->update_sensors(); }
+  void play() override { 
+    if (this->sd_card_) {
+      this->sd_card_->update_sensors(); 
+    }
+  }
   
  protected:
   SDCard *sd_card_;
 };
 
-template<typename... Ts> class SDCardUpdateTrigger : public Trigger<Ts...> {
+class SDCardUpdateTrigger : public Trigger<> {
  public:
   explicit SDCardUpdateTrigger(SDCard *sd_card) {
-    sd_card->add_on_update_callback([this]() { this->trigger(); });
+    if (sd_card) {
+      // On suppose ici que add_on_update_callback() existe et fonctionne
+      sd_card->add_on_update_callback([this]() { this->trigger(); });
+    }
   }
 };
 
 }  // namespace sd_card
 }  // namespace esphome
+
