c8c00bd08bff190ceb1c0e029fd203bd5d19b7b1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 04:32:55 +0100

    Update __init__.py

--- esphome/components/sd_box_card/__init__.py
+++ esphome/components/sd_box_card/__init__.py
@@ -8,35 +8,33 @@ DEPENDENCIES = ['esp32']
 sd_box_card_ns = cg.esphome_ns.namespace('sd_box_card')
 SDBoxCard = sd_box_card_ns.class_('SDBoxCard', cg.Component)
 
-CONF_DATA3_PIN = 'data3_pin'
-CONF_CMD_PIN = 'cmd_pin'
-CONF_DATA0_PIN = 'data0_pin'
-CONF_DATA1_PIN = 'data1_pin'
-CONF_DATA2_PIN = 'data2_pin'
+CONF_MOSI_PIN = 'mosi_pin'
+CONF_MISO_PIN = 'miso_pin'
 CONF_CLK_PIN = 'clk_pin'
-CONF_MODE_1BIT = 'mode_1bit'
+CONF_CS_PIN = 'cs_pin'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDBoxCard),
-    cv.Required(CONF_DATA3_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_CMD_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_DATA0_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_DATA1_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_DATA2_PIN): pins.gpio_output_pin_schema,
+    cv.Required(CONF_MOSI_PIN): pins.gpio_output_pin_schema,
+    cv.Required(CONF_MISO_PIN): pins.gpio_input_pin_schema,
     cv.Required(CONF_CLK_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
+    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    pins = [CONF_DATA3_PIN, CONF_CMD_PIN, CONF_DATA0_PIN, CONF_DATA1_PIN, CONF_DATA2_PIN, CONF_CLK_PIN]
-    for pin in pins:
-        pin_obj = await cg.gpio_pin_expression(config[pin])
-        cg.add(getattr(var, f"set_{pin}")(pin_obj))
+    mosi = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
+    miso = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
+    clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
+    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
 
-    cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
+    cg.add(var.set_mosi_pin(mosi))
+    cg.add(var.set_miso_pin(miso))
+    cg.add(var.set_clk_pin(clk))
+    cg.add(var.set_cs_pin(cs))
+
 
 
 
