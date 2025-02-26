9ee8bdc98b7dcf1c396268480a1998661148ed8f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 07:25:57 +0100

    Update automation.h

--- esphome/components/sd_card/automation.h
+++ esphome/components/sd_card/automation.h
@@ -1,17 +1,18 @@
 #pragma once
 #include "esphome/core/automation.h"
-#include "sd_card.h"
 #include "esphome/core/component.h"
+#include "sd_card.h"
 
 namespace esphome {
 namespace sd_card {
 
-// Action doit inclure le template correct
-class SDCardUpdateAction : public Action<> {
+// Utilisez les types corrects pour l'action
+template<typename... Ts>
+class SDCardUpdateAction : public Action<Ts...> {
  public:
   explicit SDCardUpdateAction(SDCard *sd_card) : sd_card_(sd_card) {}
   
-  void play(UpdateType type) override {
+  void play(Ts... x) override {
     if (this->sd_card_) {
       this->sd_card_->update_sensors();
     }
@@ -21,34 +22,22 @@ class SDCardUpdateAction : public Action<> {
   SDCard *sd_card_;
 };
 
-// Trigger doit inclure Component pour utiliser update()
-class SDCardUpdateTrigger : public Trigger<>, public Component {
+// Pour le trigger, héritez de PollingComponent
+class SDCardUpdateTrigger : public PollingComponent, public Trigger<> {
  public:
-  explicit SDCardUpdateTrigger(SDCard *sd_card) : sd_card_(sd_card) {
-    this->interval_ = 1000; // Intervalle de 1000ms
-  }
-  
-  void setup() override {
-    // Configuration initiale si nécessaire
-  }
+  explicit SDCardUpdateTrigger(SDCard *sd_card) : sd_card_(sd_card) {}
   
   void update() override {
-    const uint32_t now = millis();
-    if (now - this->last_update_ >= this->interval_) {
-      if (this->sd_card_) {
-        this->sd_card_->update_sensors();
-        this->trigger();  // Déclenche le trigger
-      }
-      this->last_update_ = now;
+    if (this->sd_card_) {
+      this->sd_card_->update_sensors();
+      this->trigger();
     }
   }
   
-  void set_interval(uint32_t interval) { this->interval_ = interval; }
+  float get_setup_priority() const override { return setup_priority::DATA; }
   
  protected:
   SDCard *sd_card_;
-  uint32_t interval_{1000};
-  uint32_t last_update_{0};
 };
 
 }  // namespace sd_card
