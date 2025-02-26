6a62d3b3b5214b7c56610cafe545bdf3f37b972c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 05:13:15 +0100

    Update __init__.py

--- esphome/components/sd_box_card/__init__.py
+++ esphome/components/sd_box_card/__init__.py
@@ -1,39 +1,67 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.const import CONF_ID
+from esphome.components import sensor, text_sensor
+from esphome.const import (
+    CONF_ID,
+    CONF_DATA_PIN,
+    CONF_CLK_PIN,
+    CONF_MODE,
+    DEVICE_CLASS_SD,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_MEGABYTE,
+    ICON_CARD_BULK,
+    ICON_SD,
+)
 
-DEPENDENCIES = ['esp32']
+CODEOWNERS = ["@votre_nom"]
+DEPENDENCIES = []
 
-sd_box_card_ns = cg.esphome_ns.namespace('sd_box_card')
-SDBoxCard = sd_box_card_ns.class_('SDBoxCard', cg.Component)
+sd_box_card_ns = cg.esphome_ns.namespace("sd_box_card")
+SDBoxCard = sd_box_card_ns.class_("SDBoxCard", cg.Component)
 
-CONF_MOSI_PIN = 'mosi_pin'
-CONF_MISO_PIN = 'miso_pin'
-CONF_CLK_PIN = 'clk_pin'
-CONF_CS_PIN = 'cs_pin'
+CONF_SD_BOX_CARD = "sd_box_card"
+CONF_DATA3_PIN = "data3_pin"
+CONF_CMD_PIN = "cmd_pin"
+CONF_DATA0_PIN = "data0_pin"
+CONF_DATA1_PIN = "data1_pin"
+CONF_DATA2_PIN = "data2_pin"
+CONF_MODE_1BIT = "mode_1bit"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDBoxCard),
-    cv.Required(CONF_MOSI_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_MISO_PIN): pins.gpio_input_pin_schema,
-    cv.Required(CONF_CLK_PIN): pins.gpio_output_pin_schema,
-    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
+    cv.Required(CONF_DATA3_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_CMD_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_DATA0_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_DATA1_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_DATA2_PIN): pins.gpio_input_pin_schema,
+    cv.Required(CONF_CLK_PIN): pins.gpio_input_pin_schema,
+    cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-    mosi = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
-    miso = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
-    clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
-    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-
-    cg.add(var.set_mosi_pin(mosi))
-    cg.add(var.set_miso_pin(miso))
-    cg.add(var.set_clk_pin(clk))
-    cg.add(var.set_cs_pin(cs))
+    
+    data3_pin = await cg.gpio_pin_expression(config[CONF_DATA3_PIN])
+    cg.add(var.set_data3_pin(data3_pin))
+    
+    cmd_pin = await cg.gpio_pin_expression(config[CONF_CMD_PIN])
+    cg.add(var.set_cmd_pin(cmd_pin))
+    
+    data0_pin = await cg.gpio_pin_expression(config[CONF_DATA0_PIN])
+    cg.add(var.set_data0_pin(data0_pin))
+    
+    data1_pin = await cg.gpio_pin_expression(config[CONF_DATA1_PIN])
+    cg.add(var.set_data1_pin(data1_pin))
+    
+    data2_pin = await cg.gpio_pin_expression(config[CONF_DATA2_PIN])
+    cg.add(var.set_data2_pin(data2_pin))
+    
+    clk_pin = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
+    cg.add(var.set_clk_pin(clk_pin))
+    
+    cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
 
 
 
