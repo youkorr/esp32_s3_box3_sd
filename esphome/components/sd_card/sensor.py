9588ae3a697e002e0d31c12046469dc462f1a2c0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 18:25:48 +0100

    Update sensor.py

--- esphome/components/sd_card/sensor.py
+++ esphome/components/sd_card/sensor.py
@@ -2,45 +2,60 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    DEVICE_CLASS_DATA_SIZE,
-    ENTITY_CATEGORY_DIAGNOSTIC,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_BYTE,
-    ICON_SD_CARD,
     CONF_TYPE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_BYTES,
+    ICON_MEMORY,
+)
+from . import (
+    SdMmc,
+    CONF_SD_MMC_CARD_ID,
+    CONF_PATH,
 )
 
-DEPENDENCIES = ["sd_card"]
+DEPENDENCIES = ["sd_mmc_card"]
 
-CONF_SD_CARD_ID = "sd_card_id"
-CONF_TOTAL_SPACE = "total_space"
 CONF_USED_SPACE = "used_space"
+CONF_TOTAL_SPACE = "total_space"
 CONF_FREE_SPACE = "free_space"
+CONF_FILE_SIZE = "file_size"
 
-TYPES = [CONF_TOTAL_SPACE, CONF_USED_SPACE, CONF_FREE_SPACE]
+TYPES = [CONF_USED_SPACE, CONF_TOTAL_SPACE, CONF_USED_SPACE, CONF_FREE_SPACE]
+SIMPLE_TYPES = [CONF_USED_SPACE, CONF_TOTAL_SPACE, CONF_FREE_SPACE]
 
-sd_card_ns = cg.esphome_ns.namespace("sd_card")
-SDCard = sd_card_ns.class_("SDCard", cg.Component)
-
-BASE_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_BYTE,
-    icon=ICON_SD_CARD,
+BASE_CONFIG_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=UNIT_BYTES,
+    icon=ICON_MEMORY,
     accuracy_decimals=2,
-    device_class=DEVICE_CLASS_DATA_SIZE,
     state_class=STATE_CLASS_MEASUREMENT,
-    entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-).extend({
-    cv.GenerateID(CONF_SD_CARD_ID): cv.use_id(SDCard),
-})
+).extend(
+    {
+        cv.GenerateID(CONF_SD_MMC_CARD_ID): cv.use_id(SdMmc),
+    }
+)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
-}).extend(BASE_SCHEMA)
+CONFIG_SCHEMA = cv.typed_schema(
+    {
+        CONF_TOTAL_SPACE : BASE_CONFIG_SCHEMA,
+        CONF_USED_SPACE : BASE_CONFIG_SCHEMA,
+        CONF_FREE_SPACE: BASE_CONFIG_SCHEMA,
+        CONF_FILE_SIZE: BASE_CONFIG_SCHEMA.extend(
+            {
+                cv.Required(CONF_PATH): cv.templatable(cv.string_strict),
+            }
+        )
+    },
+    lower=True,
+)
+
 
 async def to_code(config):
-    sd_card = await cg.get_variable(config[CONF_SD_CARD_ID])
+    sd_mmc_component = await cg.get_variable(config[CONF_SD_MMC_CARD_ID])
     var = await sensor.new_sensor(config)
-    func = getattr(sd_card, f"set_{config[CONF_TYPE]}_sensor")
-    cg.add(func(var))
+    if config[CONF_TYPE] in SIMPLE_TYPES:
+        func = getattr(sd_mmc_component, f"set_{config[CONF_TYPE]}_sensor")
+        cg.add(func(var))
+    elif config[CONF_TYPE] == CONF_FILE_SIZE:
+        cg.add(sd_mmc_component.add_file_size_sensor(var, config[CONF_PATH]))
 
 
