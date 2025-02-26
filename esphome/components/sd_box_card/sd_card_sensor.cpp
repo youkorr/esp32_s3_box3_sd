42acbe4fa01038169d8b09cbeee63ac1c364545e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-26 05:23:49 +0100

    Create sd_card_sensor.cpp

---
+++ esphome/components/sd_box_card/sd_card_sensor.cpp
@@ -0,0 +1,32 @@
+#include "sd_card_sensor.h"
+
+namespace esphome {
+namespace sd_box_card {
+
+static const char *TAG = "sd_card.sensor";
+
+void SDCardDiskUsageSensor::setup() {
+  if(!this->parent_->is_failed()) {
+    this->publish_state(0);
+  }
+}
+
+void SDCardDiskUsageSensor::update() {
+  if(this->parent_->is_failed()) return;
+
+  float value = 0;
+  if(this->type_ == "free") {
+    value = this->parent_->get_free_space();
+  } else if(this->type_ == "used") {
+    value = this->parent_->get_used_space();
+  } else if(this->type_ == "total") {
+    value = this->parent_->get_total_space();
+  }
+  
+  this->publish_state(value);
+}
+
+void SDCardDiskUsageSensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "SD Card Usage Sensor:");
+  ESP_LOGCONFIG(TAG, "  Type: %s", this->type_.c_str());
+}
