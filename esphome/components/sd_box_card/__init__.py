e5830b6668516495bc85360cec61d42a5d98d69e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-02 02:15:05 +0100

    Update __init__.py

--- esphome/components/sd_box_card/__init__.py
+++ esphome/components/sd_box_card/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.components import sensor, text_sensor
 from esphome.const import (
     CONF_ID,
     CONF_DATA,
