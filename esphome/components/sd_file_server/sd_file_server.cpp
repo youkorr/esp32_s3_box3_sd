812494bddd935f023d4725485b2d2391377970cc
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-10 05:15:51 +0100

    Update sd_file_server.cpp

--- esphome/components/sd_file_server/sd_file_server.cpp
+++ esphome/components/sd_file_server/sd_file_server.cpp
@@ -2,6 +2,8 @@
 #include "esphome/core/log.h"
 #include "esphome/components/sd_mmc_card/sd_mmc_card.h"
 #include "esphome/components/web_server_base/web_server_base.h"
+#include "FS.h"   // Ajoutez cette ligne pour la gestion des fichiers
+#include "SD.h"   // Ajoutez cette ligne si vous utilisez la bibliothèque SD
 
 namespace esphome {
 namespace sd_file_server {
@@ -23,13 +25,13 @@ void SDFileServer::dump_config() {
 }
 
 bool SDFileServer::canHandle(AsyncWebServerRequest *request) {
-  // Si la requête correspond au préfixe URL et à la méthode POST, on gère l'upload
-  return request->url().startsWith("/" + this->url_prefix_) && request->method() == HTTP_POST;
+  // Remplacez startsWith par starts_with
+  return request->url().starts_with("/" + this->url_prefix_) && request->method() == HTTP_POST;
 }
 
 void SDFileServer::handleRequest(AsyncWebServerRequest *request) {
-  // Le gestionnaire de requêtes appelle handleUpload si la méthode POST est utilisée pour l'upload de fichiers
-  if (this->upload_enabled_ && request->url().startsWith("/" + this->url_prefix_)) {
+  // Si l'upload est activé et que l'URL commence par le préfixe, nous traitons l'upload
+  if (this->upload_enabled_ && request->url().starts_with("/" + this->url_prefix_)) {
     request->send(200, "text/plain", "Send file to upload");
   } else {
     request->send(404, "text/plain", "Not Found");
@@ -38,7 +40,7 @@ void SDFileServer::handleRequest(AsyncWebServerRequest *request) {
 
 void SDFileServer::handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len,
                                 bool final) {
-  // Vérifier si l'upload est activé
+  // Vérifiez si l'upload est activé
   if (!this->upload_enabled_) {
     request->send(400, "text/plain", "Upload is not enabled");
     return;
@@ -48,7 +50,7 @@ void SDFileServer::handleUpload(AsyncWebServerRequest *request, const String &fi
   std::string file_path = this->build_absolute_path(filename.c_str());
 
   // Ouvrir le fichier pour écrire
-  File file = SD_MMC.open(file_path.c_str(), FILE_WRITE);
+  File file = SD_MMC.open(file_path.c_str(), FILE_WRITE);  // Utilisez SD_MMC ici
   if (!file) {
     ESP_LOGE(TAG, "Failed to open file %s for writing", file_path.c_str());
     request->send(500, "text/plain", "Failed to open file for writing");
@@ -95,7 +97,6 @@ std::string SDFileServer::build_prefix() const {
 }
 
 std::string SDFileServer::extract_path_from_url(std::string const &url) const {
-  // Extrait le chemin du fichier à partir de l'URL (supprime le préfixe et les autres parties)
   std::string path = url;
   if (path.find(this->url_prefix_) == 0) {
     path = path.substr(this->url_prefix_.length());
@@ -104,30 +105,26 @@ std::string SDFileServer::extract_path_from_url(std::string const &url) const {
 }
 
 std::string SDFileServer::build_absolute_path(std::string file_name) const {
-  // Construire un chemin absolu pour le fichier
   return this->root_path_ + "/" + file_name;
 }
 
 void SDFileServer::write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const {
-  // Fonction pour écrire une ligne dans la réponse, utilisée pour l'affichage des fichiers
-  response->printf("<tr><td>%s</td><td>%s</td><td><a href='/delete/%s'>Delete</a></td></tr>", info.name.c_str(), info.size.c_str(), info.name.c_str());
+  // Corrigez l'accès aux membres `name` et `size` de `FileInfo`
+  response->printf("<tr><td>%s</td><td>%u</td><td><a href='/delete/%s'>Delete</a></td></tr>", info.get_name().c_str(), info.get_size(), info.get_name().c_str());
 }
 
 void SDFileServer::handle_index(AsyncWebServerRequest *request, std::string const &path) const {
-  // Fonction pour gérer la page d'index (généralement utilisée pour l'affichage de la liste des fichiers)
 }
 
 void SDFileServer::handle_get(AsyncWebServerRequest *request) const {
-  // Fonction pour gérer les requêtes GET
 }
 
 void SDFileServer::handle_delete(AsyncWebServerRequest *request) {
-  // Fonction pour gérer les suppressions de fichiers
 }
 
 void SDFileServer::handle_download(AsyncWebServerRequest *request, std::string const &file_name) const {
-  // Fonction pour gérer le téléchargement de fichiers
 }
 
 }  // namespace sd_file_server
 }  // namespace esphome
+
