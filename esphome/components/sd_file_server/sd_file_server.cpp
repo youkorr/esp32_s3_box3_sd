80a3e4cfffa3955f89d7d925e0fdcb5d852958d0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-12 17:08:09 +0100

    Update sd_file_server.cpp

--- esphome/components/sd_file_server/sd_file_server.cpp
+++ esphome/components/sd_file_server/sd_file_server.cpp
@@ -263,7 +263,7 @@ void SDFileServer::handle_download(AsyncWebServerRequest *request, std::string c
   
   while ((bytes_read = fread(buffer, 1, sizeof(buffer), f)) > 0) {
     // Use the correct method for ESP-IDF
-    response->write((const char*)buffer, bytes_read);
+    response->send((const char*)buffer, bytes_read);
   }
   
   fclose(f);
