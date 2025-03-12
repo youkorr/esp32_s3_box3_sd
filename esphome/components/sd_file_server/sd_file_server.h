72c2a7efe28b30fca05b172705e5edeca2f07dba
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-12 18:01:15 +0100

    Update sd_file_server.h

--- esphome/components/sd_file_server/sd_file_server.h
+++ esphome/components/sd_file_server/sd_file_server.h
@@ -46,13 +46,9 @@ class SDFileServer : public Component, public AsyncWebHandler {
 
 struct Path {
   static constexpr char separator = '/';
-  /* Return the name of the file */
   static std::string file_name(std::string const &);
-  /* Is the path an absolute path? */
   static bool is_absolute(std::string const &);
-  /* Does the path have a trailing slash? */
   static bool trailing_slash(std::string const &);
-  /* Join two path */
   static std::string join(std::string const &, std::string const &);
   static std::string remove_root_path(std::string path, std::string const &root);
 };
