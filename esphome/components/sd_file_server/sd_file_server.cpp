0742e6ba0d3d829cc371f2e7047a471d9ce0f5fb
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-12 01:03:57 +0100

    Update sd_file_server.cpp

--- esphome/components/sd_file_server/sd_file_server.cpp
+++ esphome/components/sd_file_server/sd_file_server.cpp
@@ -6,10 +6,90 @@
 namespace esphome {
 namespace sd_file_server {
 
-SDFileServer::SDFileServer(web_server_base::WebServerBase *base) : base_(base) {
-  this->deletion_enabled_ = false;
-  this->download_enabled_ = true;
-  this->upload_enabled_ = true;
+// Ajout de la structure FileInfo
+struct FileInfo {
+  std::string filename;
+  size_t size;
+};
+
+// Ajout de la méthode list_files dans SdMmc
+class SdMmc {
+ public:
+  std::vector<FileInfo> list_files(const std::string &path) {
+    std::vector<FileInfo> files;
+    // Implémentation de la lecture des fichiers
+    return files;
+  }
+
+  bool is_directory(const std::string &path) {
+    // Implémentation de la vérification de répertoire
+    return false;
+  }
+};
+
+// Modification de write_row
+void SDFileServer::write_row(AsyncResponseStream *response, const FileInfo &info) const {
+  response->print("<tr>");
+  response->printf("<td>%s</td>", info.filename.c_str());
+  response->printf("<td>%d</td>", info.size);
+  response->print("</tr>");
+}
+
+// Modification de handle_index
+void SDFileServer::handle_index(AsyncWebServerRequest *request, std::string const &path) const {
+  auto *response = request->beginResponseStream("text/html");
+  response->print("<html><head><title>SD File Server</title></head><body>");
+  response->print("<h1>SD Card Contents</h1>");
+  response->print("<table border='1'>");
+  response->print("<tr><th>Name</th><th>Size</th></tr>");
+
+  auto files = this->sd_mmc_card_->list_files(path);
+  for (auto const &file : files) {
+    this->write_row(response, file);
+  }
+
+  response->print("</table></body></html>");
+  request->send(response);
+}
+
+SDFileServer::SDFileServer(web_server_base::WebServerBase *base) : base_(base) {}
+
+void SDFileServer::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up SD File Server...");
+  this->base_->init();
+  this->base_->add_handler(this);
+}
+
+void SDFileServer::dump_config() {
+  ESP_LOGCONFIG(TAG, "SD File Server:");
+  ESP_LOGCONFIG(TAG, "  URL Prefix: %s", this->url_prefix_.c_str());
+  ESP_LOGCONFIG(TAG, "  Root Path: %s", this->root_path_.c_str());
+  ESP_LOGCONFIG(TAG, "  Deletion Enabled: %s", YESNO(this->deletion_enabled_));
+  ESP_LOGCONFIG(TAG, "  Download Enabled: %s", YESNO(this->download_enabled_));
+  ESP_LOGCONFIG(TAG, "  Upload Enabled: %s", YESNO(this->upload_enabled_));
+}
+
+bool SDFileServer::canHandle(AsyncWebServerRequest *request) {
+  return request->url().rfind(this->build_prefix(), 0) == 0;
+}
+
+void SDFileServer::handleRequest(AsyncWebServerRequest *request) {
+  std::string path = this->extract_path_from_url(request->url().c_str());
+  std::string absolute_path = this->build_absolute_path(path);
+
+  if (request->method() == HTTP_GET) {
+    if (path.empty() || this->sd_mmc_card_->is_directory(absolute_path)) {
+      this->handle_index(request, path);
+    } else {
+      this->handle_get(request);
+    }
+  } else if (request->method() == HTTP_DELETE && this->deletion_enabled_) {
+    this->handle_delete(request);
+  }
+}
+
+void SDFileServer::handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) {
+  // Implementation de l'upload
 }
 
 void SDFileServer::set_url_prefix(std::string const &url_prefix) {
@@ -37,76 +117,20 @@ void SDFileServer::set_upload_enabled(bool upload_enabled) {
 }
 
 std::string SDFileServer::build_prefix() const {
-  return this->url_prefix_ + "/";
+  return this->url_prefix_;
 }
 
 std::string SDFileServer::extract_path_from_url(std::string const &url) const {
-  if (url.rfind(this->url_prefix_, 0) != 0)
-    return "";
-
-  return url.substr(this->url_prefix_.length());
+  return url.substr(this->build_prefix().length());
 }
 
 std::string SDFileServer::build_absolute_path(std::string path) const {
-  if (path.empty() || path == "/")
-    return this->root_path_;
-
-  if (path[0] == '/')
-    path = path.substr(1);
-
   return Path::join(this->root_path_, path);
-}
-
-void SDFileServer::write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const {
-  response->print("<tr>");
-  response->printf("<td>%s</td>", info.name.c_str());
-  response->printf("<td>%d</td>", info.size);
-  response->print("</tr>");
-}
-
-void SDFileServer::handle_index(AsyncWebServerRequest *request, std::string const &path) const {
-  auto *response = request->beginResponseStream("text/html");
-  response->print("<html><head><title>SD File Server</title></head><body>");
-  response->print("<h1>SD Card Contents</h1>");
-  response->print("<table border='1'>");
-  response->print("<tr><th>Name</th><th>Size</th></tr>");
-
-  auto files = this->sd_mmc_card_->list_files(path);
-  for (auto const &file : files) {
-    this->write_row(response, file);
-  }
-
-  response->print("</table></body></html>");
-  request->send(response);
-}
-
-void SDFileServer::handle_get(AsyncWebServerRequest *request) const {
-  std::string path = this->extract_path_from_url(request->url().c_str());
-  std::string absolute_path = this->build_absolute_path(path);
-
-  if (this->sd_mmc_card_->is_directory(absolute_path)) {
-    this->handle_index(request, absolute_path);
-    return;
-  }
-
-  this->handle_download(request, path);
-}
-
-void SDFileServer::handleRequest(AsyncWebServerRequest *request) {
-  if (request->method() == HTTP_GET) {
-    this->handle_get(request);
-  } else if (request->method() == HTTP_DELETE) {
-    this->handle_delete(request);
-  } else {
-    request->send(405);
-  }
 }
 
 std::string Path::file_name(std::string const &path) {
   size_t pos = path.find_last_of(separator);
-  if (pos == std::string::npos)
-    return path;
-  return path.substr(pos + 1);
+  return pos == std::string::npos ? path : path.substr(pos + 1);
 }
 
 bool Path::is_absolute(std::string const &path) {
@@ -118,27 +142,22 @@ bool Path::trailing_slash(std::string const &path) {
 }
 
 std::string Path::join(std::string const &path1, std::string const &path2) {
-  if (path1.empty())
-    return path2;
-  if (path2.empty())
-    return path1;
-
+  if (path1.empty()) return path2;
+  if (path2.empty()) return path1;
+  
   std::string result = path1;
-  if (result.back() != separator)
+  if (!trailing_slash(path1) && !is_absolute(path2)) {
     result += separator;
-  if (path2.front() == separator)
-    result += path2.substr(1);
-  else
-    result += path2;
-
+  }
+  result += path2;
   return result;
 }
 
 std::string Path::remove_root_path(std::string path, std::string const &root) {
-  if (path.rfind(root, 0) == 0)
-    return path;
-
-  return path.substr(root.length());
+  if (path.rfind(root, 0) == 0) {
+    return path.substr(root.length());
+  }
+  return path;
 }
 
 }  // namespace sd_file_server
