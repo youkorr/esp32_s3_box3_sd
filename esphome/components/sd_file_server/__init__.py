7107e2387b590b0756630ecc054795a8f5b4e3d3
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-20 19:40:23 +0100

    Update __init__.py

--- esphome/components/sd_file_server/__init__.py
+++ esphome/components/sd_file_server/__init__.py
@@ -1,6 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.components import web_server_base
+from esphome.components.web_server_base import CONF_WEB_SERVER_BASE_ID
+from esphome.const import (
+    CONF_ID
+)
 from esphome.core import coroutine_with_priority, CORE
 from .. import sd_mmc_card
 
@@ -9,8 +13,8 @@ CONF_ROOT_PATH = "root_path"
 CONF_ENABLE_DELETION = "enable_deletion"
 CONF_ENABLE_DOWNLOAD = "enable_download"
 CONF_ENABLE_UPLOAD = "enable_upload"
-CONF_PORT = "port"
 
+AUTO_LOAD = ["web_server_base"]
 DEPENDENCIES = ["sd_mmc_card"]
 
 sd_file_server_ns = cg.esphome_ns.namespace("sd_file_server")
@@ -20,12 +24,14 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(SDFileServer),
+            cv.GenerateID(CONF_WEB_SERVER_BASE_ID): cv.use_id(
+                web_server_base.WebServerBase
+            ),
             cv.GenerateID(sd_mmc_card.CONF_SD_MMC_CARD_ID): cv.use_id(sd_mmc_card.SdMmc),
-            cv.Optional(CONF_PORT, default=80): cv.port,
-            cv.Optional(CONF_URL_PREFIX, default="files"): cv.string_strict,
-            cv.Optional(CONF_ROOT_PATH, default="/sdcard"): cv.string_strict,
+            cv.Optional(CONF_URL_PREFIX, default="file"): cv.string_strict,
+            cv.Optional(CONF_ROOT_PATH, default="/"): cv.string_strict,
             cv.Optional(CONF_ENABLE_DELETION, default=False): cv.boolean,
-            cv.Optional(CONF_ENABLE_DOWNLOAD, default=True): cv.boolean,
+            cv.Optional(CONF_ENABLE_DOWNLOAD, default=False): cv.boolean,
             cv.Optional(CONF_ENABLE_UPLOAD, default=False): cv.boolean,
         }
     ).extend(cv.COMPONENT_SCHEMA),
@@ -33,12 +39,12 @@ CONFIG_SCHEMA = cv.All(
 
 @coroutine_with_priority(45.0)
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    paren = await cg.get_variable(config[CONF_WEB_SERVER_BASE_ID])
     
+    var = cg.new_Pvariable(config[CONF_ID], paren)
+    await cg.register_component(var, config)
     sdmmc = await cg.get_variable(config[sd_mmc_card.CONF_SD_MMC_CARD_ID])
     cg.add(var.set_sd_mmc_card(sdmmc))
-    cg.add(var.set_port(config[CONF_PORT]))
     cg.add(var.set_url_prefix(config[CONF_URL_PREFIX]))
     cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
     cg.add(var.set_deletion_enabled(config[CONF_ENABLE_DELETION]))
