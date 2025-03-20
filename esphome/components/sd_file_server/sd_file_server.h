5cb23c6dcb2133df8b73ccdf2e19b9dd0090fff5
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-20 17:45:24 +0100

    Update sd_file_server.h

--- esphome/components/sd_file_server/sd_file_server.h
+++ esphome/components/sd_file_server/sd_file_server.h
@@ -1,91 +1,73 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/components/web_server_base/web_server_base.h"
-#include "esphome/components/sd_mmc_card/sd_mmc_card.h"
-
-#ifdef USE_ESP_IDF
-#include "esp_http_server.h"
-#endif
+#include "../sd_mmc_card/sd_mmc_card.h"
 
 namespace esphome {
 namespace sd_file_server {
 
-class SDFileServer : public Component {
-public:
-    explicit SDFileServer(web_server_base::WebServerBase *base);
-#ifdef USE_ESP_IDF
-    explicit SDFileServer(httpd_handle_t server);
-#endif
+class SDFileServer : public Component, public AsyncWebHandler {
+ public:
+  SDFileServer(web_server_base::WebServerBase *);
+  void setup() override;
+  void dump_config() override;
+  bool canHandle(AsyncWebServerRequest *request) override;
+  void handleRequest(AsyncWebServerRequest *request) override;
+  void handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len,
+                    bool final) override;
+  bool isRequestHandlerTrivial() override { return false; }
 
-    void setup() override;
-    void dump_config() override;
+  void set_url_prefix(std::string const &);
+  void set_root_path(std::string const &);
+  void set_sd_mmc_card(sd_mmc_card::SdMmc *);
+  void set_deletion_enabled(bool);
+  void set_download_enabled(bool);
+  void set_upload_enabled(bool);
 
-#ifdef USE_ESP_IDF
-    static esp_err_t handle_get_static(httpd_req_t *req);
-    static esp_err_t handle_delete_static(httpd_req_t *req);
-    static esp_err_t handle_post_static(httpd_req_t *req);
+ protected:
+  web_server_base::WebServerBase *base_;
+  sd_mmc_card::SdMmc *sd_mmc_card_;
 
-    esp_err_t handle_get_idf(httpd_req_t *req);
-    esp_err_t handle_delete_idf(httpd_req_t *req);
-    esp_err_t handle_post_idf(httpd_req_t *req);
-    esp_err_t handle_download_idf(httpd_req_t *req, const std::string &path) const;
-    esp_err_t handle_index_idf(httpd_req_t *req, const std::string &path) const;
-#else
-    bool canHandle(AsyncWebServerRequest *request) override;
-    void handleRequest(AsyncWebServerRequest *request) override;
-    void handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data,
-                      size_t len, bool final) override;
-    bool isRequestHandlerTrivial() override { return false; }
-#endif
+  std::string url_prefix_;
+  std::string root_path_;
+  bool deletion_enabled_;
+  bool download_enabled_;
+  bool upload_enabled_;
 
-    void set_url_prefix(const std::string &);
-    void set_root_path(const std::string &);
-    void set_sd_mmc_card(sd_mmc_card::SdMmc *);
-    void set_deletion_enabled(bool);
-    void set_download_enabled(bool);
-    void set_upload_enabled(bool);
-
-protected:
-#ifdef USE_ESP_IDF
-    httpd_handle_t server_ = nullptr;
-#else
-    web_server_base::WebServerBase *base_ = nullptr;
-#endif
-
-    sd_mmc_card::SdMmc *sd_mmc_card_ = nullptr;
-
-    std::string url_prefix_;
-    std::string root_path_;
-    bool deletion_enabled_ = false;
-    bool download_enabled_ = false;
-    bool upload_enabled_ = false;
-
-    std::string build_prefix() const;
-    std::string extract_path_from_url(const std::string &) const;
-    std::string build_absolute_path(const std::string &) const;
-    void write_row(AsyncResponseStream *response, const sd_mmc_card::FileInfo &info) const;
-    void handle_index(AsyncWebServerRequest *, const std::string &) const;
-    void handle_get(AsyncWebServerRequest *) const;
-    void handle_delete(AsyncWebServerRequest *);
-    void handle_download(AsyncWebServerRequest *, const std::string &) const;
-
-    bool write_file_chunked(const std::string &path, const std::function<size_t(uint8_t *, size_t)> &data_provider,
-                            size_t chunk_size);
+  std::string build_prefix() const;
+  std::string extract_path_from_url(std::string const &) const;
+  std::string build_absolute_path(std::string) const;
+  void write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const;
+  void handle_index(AsyncWebServerRequest *, std::string const &) const;
+  void handle_get(AsyncWebServerRequest *) const;
+  void handle_delete(AsyncWebServerRequest *);
+  void handle_download(AsyncWebServerRequest *, std::string const &) const;
 };
 
 struct Path {
-    static constexpr char separator = '/';
+  static constexpr char separator = '/';
 
-    static std::string file_name(const std::string &);
-    static bool is_absolute(const std::string &);
-    static bool trailing_slash(const std::string &);
-    static std::string join(const std::string &, const std::string &);
-    static std::string remove_root_path(std::string, const std::string &);
-    static std::vector<std::string> split_path(const std::string &);
-    static std::string extension(const std::string &);
-    static std::string file_type(const std::string &);
-    static std::string mime_type(const std::string &);
+  /* Return the name of the file */
+  static std::string file_name(std::string const &);
+
+  /* Is the path an absolute path? */
+  static bool is_absolute(std::string const &);
+
+  /* Does the path have a trailing slash? */
+  static bool trailing_slash(std::string const &);
+
+  /* Join two path */
+  static std::string join(std::string const &, std::string const &);
+
+  static std::string remove_root_path(std::string path, std::string const &root);
+
+  static std::vector<std::string> split_path(std::string path);
+
+  static std::string extension(std::string const &);
+
+  static std::string file_type(std::string const &);
+
+  static std::string mime_type(std::string const &);
 };
 
 }  // namespace sd_file_server
