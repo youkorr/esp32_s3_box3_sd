9fc2d2cc1417fa7f93aeea46ef6141f594b54ac0
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-20 22:09:03 +0100

    Update __init__.py

--- esphome/components/sd_file_server/__init__.py
+++ esphome/components/sd_file_server/__init__.py
@@ -1,54 +1,152 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import web_server_base
-from esphome.components.web_server_base import CONF_WEB_SERVER_BASE_ID
+from esphome import automation, pins
 from esphome.const import (
-    CONF_ID
+    CONF_ID,
+    CONF_DATA,
+    CONF_PATH,
+    CONF_CLK_PIN,
+    CONF_INPUT,
+    CONF_OUTPUT,
+    
 )
-from esphome.core import coroutine_with_priority, CORE
-from .. import sd_mmc_card
+from esphome.core import CORE
+CONF_POWER_CTRL_PIN = "power_ctrl_pin"
+CONF_SD_MMC_CARD_ID = "sd_mmc_card_id"
+CONF_CMD_PIN = "cmd_pin"
+CONF_DATA0_PIN = "data0_pin"
+CONF_DATA1_PIN = "data1_pin"
+CONF_DATA2_PIN = "data2_pin"
+CONF_DATA3_PIN = "data3_pin"
+CONF_MODE_1BIT = "mode_1bit"
 
-CONF_URL_PREFIX = "url_prefix"
-CONF_ROOT_PATH = "root_path"
-CONF_ENABLE_DELETION = "enable_deletion"
-CONF_ENABLE_DOWNLOAD = "enable_download"
-CONF_ENABLE_UPLOAD = "enable_upload"
+sd_mmc_card_component_ns = cg.esphome_ns.namespace("sd_mmc_card")
+SdMmc = sd_mmc_card_component_ns.class_("SdMmc", cg.Component)
 
-AUTO_LOAD = ["web_server_base"]
-DEPENDENCIES = ["sd_mmc_card"]
+# Action
+SdMmcWriteFileAction = sd_mmc_card_component_ns.class_("SdMmcWriteFileAction", automation.Action)
+SdMmcAppendFileAction = sd_mmc_card_component_ns.class_("SdMmcAppendFileAction", automation.Action)
+SdMmcCreateDirectoryAction = sd_mmc_card_component_ns.class_("SdMmcCreateDirectoryAction", automation.Action)
+SdMmcRemoveDirectoryAction = sd_mmc_card_component_ns.class_("SdMmcRemoveDirectoryAction", automation.Action)
+SdMmcDeleteFileAction = sd_mmc_card_component_ns.class_("SdMmcDeleteFileAction", automation.Action)
 
-sd_file_server_ns = cg.esphome_ns.namespace("sd_file_server")
-SDFileServer = sd_file_server_ns.class_("SDFileServer", cg.Component)
+def validate_raw_data(value):
+    if isinstance(value, str):
+        return value.encode("utf-8")
+    if isinstance(value, list):
+        return cv.Schema([cv.hex_uint8_t])(value)
+    raise cv.Invalid(
+        "data must either be a string wrapped in quotes or a list of bytes"
+    )
 
-CONFIG_SCHEMA = cv.All(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(SDFileServer),
-            cv.GenerateID(CONF_WEB_SERVER_BASE_ID): cv.use_id(
-                web_server_base.WebServerBase
-            ),
-            cv.GenerateID(sd_mmc_card.CONF_SD_MMC_CARD_ID): cv.use_id(sd_mmc_card.SdMmc),
-            cv.Optional(CONF_URL_PREFIX, default="file"): cv.string_strict,
-            cv.Optional(CONF_ROOT_PATH, default="/"): cv.string_strict,
-            cv.Optional(CONF_ENABLE_DELETION, default=False): cv.boolean,
-            cv.Optional(CONF_ENABLE_DOWNLOAD, default=False): cv.boolean,
-            cv.Optional(CONF_ENABLE_UPLOAD, default=False): cv.boolean,
-        }
-    ).extend(cv.COMPONENT_SCHEMA),
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(SdMmc),
+        cv.Required(CONF_CLK_PIN): pins.internal_gpio_output_pin_number,
+        cv.Required(CONF_CMD_PIN): pins.internal_gpio_output_pin_number,
+        cv.Required(CONF_DATA0_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
+        cv.Optional(CONF_DATA1_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
+        cv.Optional(CONF_DATA2_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
+        cv.Optional(CONF_DATA3_PIN): pins.internal_gpio_pin_number({CONF_OUTPUT: True, CONF_INPUT: True}),
+        cv.Optional(CONF_MODE_1BIT, default=False): cv.boolean,
+        cv.Optional(CONF_POWER_CTRL_PIN): pins.internal_gpio_output_pin_number,
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
-@coroutine_with_priority(45.0)
 async def to_code(config):
-    paren = await cg.get_variable(config[CONF_WEB_SERVER_BASE_ID])
-    
-    var = cg.new_Pvariable(config[CONF_ID], paren)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    sdmmc = await cg.get_variable(config[sd_mmc_card.CONF_SD_MMC_CARD_ID])
-    cg.add(var.set_sd_mmc_card(sdmmc))
-    cg.add(var.set_url_prefix(config[CONF_URL_PREFIX]))
-    cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
-    cg.add(var.set_deletion_enabled(config[CONF_ENABLE_DELETION]))
-    cg.add(var.set_download_enabled(config[CONF_ENABLE_DOWNLOAD]))
-    cg.add(var.set_upload_enabled(config[CONF_ENABLE_UPLOAD]))
-    
-    cg.add_define("USE_SD_CARD_WEBSERVER")
+
+    cg.add(var.set_mode_1bit(config[CONF_MODE_1BIT]))
+
+    cg.add(var.set_clk_pin(config[CONF_CLK_PIN]))
+    cg.add(var.set_cmd_pin(config[CONF_CMD_PIN]))
+    cg.add(var.set_data0_pin(config[CONF_DATA0_PIN]))
+
+    if (config[CONF_MODE_1BIT] == False):
+        cg.add(var.set_data1_pin(config[CONF_DATA1_PIN]))
+        cg.add(var.set_data2_pin(config[CONF_DATA2_PIN]))
+        cg.add(var.set_data3_pin(config[CONF_DATA3_PIN]))
+
+    if CONF_POWER_CTRL_PIN in config:
+        cg.add(var.set_power_ctrl_pin(config[CONF_POWER_CTRL_PIN]))
+
+    if CORE.using_arduino:
+        if CORE.is_esp32:
+            cg.add_library("FS", None)
+            cg.add_library("SD_MMC", None)
+
+
+SD_MMC_PATH_ACTION_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.use_id(SdMmc),
+        cv.Required(CONF_PATH): cv.templatable(cv.string_strict),
+    }
+)
+
+SD_MMC_WRITE_FILE_ACTION_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.use_id(SdMmc),
+        cv.Required(CONF_PATH): cv.templatable(cv.string_strict),
+        cv.Required(CONF_DATA): cv.templatable(validate_raw_data),
+    }
+).extend(SD_MMC_PATH_ACTION_SCHEMA)
+
+@automation.register_action(
+    "sd_mmc_card.write_file", SdMmcWriteFileAction, SD_MMC_WRITE_FILE_ACTION_SCHEMA
+)
+async def sd_mmc_write_file_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+    path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
+    data_ = await cg.templatable(config[CONF_DATA], args, cg.std_vector.template(cg.uint8))
+    cg.add(var.set_path(path_))
+    cg.add(var.set_data(data_))
+    return var
+
+
+@automation.register_action(
+    "sd_mmc_card.append_file", SdMmcAppendFileAction, SD_MMC_WRITE_FILE_ACTION_SCHEMA
+)
+async def sd_mmc_append_file_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+    path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
+    data_ = await cg.templatable(config[CONF_DATA], args, cg.std_vector.template(cg.uint8))
+    cg.add(var.set_path(path_))
+    cg.add(var.set_data(data_))
+    return var
+
+
+@automation.register_action(
+    "sd_mmc_card.create_directory", SdMmcCreateDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA
+)
+async def sd_mmc_create_directory_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+    path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
+    cg.add(var.set_path(path_))
+    return var
+
+
+@automation.register_action(
+    "sd_mmc_card.remove_directory", SdMmcRemoveDirectoryAction, SD_MMC_PATH_ACTION_SCHEMA
+)
+async def sd_mmc_remove_directory_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+    path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
+    cg.add(var.set_path(path_))
+    return var
+
+
+@automation.register_action(
+    "sd_mmc_card.delete_file", SdMmcDeleteFileAction, SD_MMC_PATH_ACTION_SCHEMA
+)
+async def sd_mmc_delete_file_to_code(config, action_id, template_arg, args):
+    parent = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, parent)
+    path_ = await cg.templatable(config[CONF_PATH], args, cg.std_string)
+    cg.add(var.set_path(path_))
+    return var
+
