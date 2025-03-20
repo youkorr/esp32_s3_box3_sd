6d6454657b299f87c9b9a2b00e0903ba234feab7
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-20 19:49:54 +0100

    Update sd_file_server.h

--- esphome/components/sd_file_server/sd_file_server.h
+++ esphome/components/sd_file_server/sd_file_server.h
@@ -1,47 +1,73 @@
 #pragma once
-
 #include "esphome/core/component.h"
-#include "esphome/components/sd_mmc_card/sd_mmc_card.h"
-#include <esp_http_server.h>
+#include "esphome/components/web_server_base/web_server_base.h"
+#include "../sd_mmc_card/sd_mmc_card.h"
 
 namespace esphome {
 namespace sd_file_server {
 
-class SDFileServer : public Component {
+class SDFileServer : public Component, public AsyncWebHandler {
  public:
-  SDFileServer();
-
+  SDFileServer(web_server_base::WebServerBase *);
   void setup() override;
-  void loop() override;
   void dump_config() override;
+  bool canHandle(AsyncWebServerRequest *request) override;
+  void handleRequest(AsyncWebServerRequest *request) override;
+  void handleUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len,
+                    bool final) override;
+  bool isRequestHandlerTrivial() override { return false; }
 
-  void set_port(uint16_t port) { port_ = port; }
-  void set_url_prefix(const std::string &prefix) { url_prefix_ = prefix; }
-  void set_root_path(const std::string &path) { root_path_ = path; }
-  void set_sd_mmc_card(sd_mmc_card::SdMmc *card) { sd_mmc_card_ = card; }
-  void set_deletion_enabled(bool enabled) { deletion_enabled_ = enabled; }
-  void set_download_enabled(bool enabled) { download_enabled_ = enabled; }
-  void set_upload_enabled(bool enabled) { upload_enabled_ = enabled; }
+  void set_url_prefix(std::string const &);
+  void set_root_path(std::string const &);
+  void set_sd_mmc_card(sd_mmc_card::SdMmc *);
+  void set_deletion_enabled(bool);
+  void set_download_enabled(bool);
+  void set_upload_enabled(bool);
 
  protected:
-  httpd_handle_t server_{nullptr};
-  sd_mmc_card::SdMmc *sd_mmc_card_{nullptr};
-  uint16_t port_{80};
-  std::string url_prefix_{"files"};
-  std::string root_path_{"/sdcard"};
-  bool deletion_enabled_{false};
-  bool download_enabled_{true};
-  bool upload_enabled_{false};
+  web_server_base::WebServerBase *base_;
+  sd_mmc_card::SdMmc *sd_mmc_card_;
 
-  void start_server();
-  static esp_err_t index_handler(httpd_req_t *req);
-  static esp_err_t file_handler(httpd_req_t *req);
-  static esp_err_t delete_handler(httpd_req_t *req);
-  static esp_err_t upload_handler(httpd_req_t *req);
+  std::string url_prefix_;
+  std::string root_path_;
+  bool deletion_enabled_;
+  bool download_enabled_;
+  bool upload_enabled_;
 
   std::string build_prefix() const;
-  std::string extract_path_from_url(const std::string &url) const;
-  std::string build_absolute_path(const std::string &file_path) const;
+  std::string extract_path_from_url(std::string const &) const;
+  std::string build_absolute_path(std::string) const;
+  void write_row(AsyncResponseStream *response, sd_mmc_card::FileInfo const &info) const;
+  void handle_index(AsyncWebServerRequest *, std::string const &) const;
+  void handle_get(AsyncWebServerRequest *) const;
+  void handle_delete(AsyncWebServerRequest *);
+  void handle_download(AsyncWebServerRequest *, std::string const &) const;
+};
+
+struct Path {
+  static constexpr char separator = '/';
+
+  /* Return the name of the file */
+  static std::string file_name(std::string const &);
+
+  /* Is the path an absolute path? */
+  static bool is_absolute(std::string const &);
+
+  /* Does the path have a trailing slash? */
+  static bool trailing_slash(std::string const &);
+
+  /* Join two path */
+  static std::string join(std::string const &, std::string const &);
+
+  static std::string remove_root_path(std::string path, std::string const &root);
+
+  static std::vector<std::string> split_path(std::string path);
+
+  static std::string extension(std::string const &);
+
+  static std::string file_type(std::string const &);
+
+  static std::string mime_type(std::string const &);
 };
 
 }  // namespace sd_file_server
